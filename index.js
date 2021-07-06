e95cd96ec7bf526e0ecaa4fd2c4cfff5190de15e
Fabio <fabiohigoralm2@gmail.com>
2021-07-06 18:48:35 -0300

    PORT

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const strangerThingsService = new StrangerThingsService(
   strangerThingsRepository,
 );
 
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 const hereIsTheUpsideDown = true;
@@ -27,6 +28,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
